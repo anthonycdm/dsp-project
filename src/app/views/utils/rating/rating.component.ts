c82a835921f7d3a587799d33e8de060c7ffaea86
Anthony CIREDEM <anthonyciredem@gmail.com>
2017-12-13 20:33:04 +0100

    Commit : Début notes page activite

--- src/app/views/utils/rating/rating.component.ts
+++ src/app/views/utils/rating/rating.component.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Commentaires } from '../../../models/index';
+import * as myGlobals from '../../../globals/index';
+import { NotesService, AuthService } from '../../../services/index';
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
- currentRate = 0;
-  constructor(config: NgbRatingConfig) {
-    config.max = 5;
-    config.resettable = true;
+ etoile : Number = 0;
+ isLoggedIn = this.auth.isLoggedIn();
+
+ constructor( private config: NgbRatingConfig,
+              private note : NotesService,
+              private auth : AuthService) {
+              config.max = 5;
+              config.resettable = true;
+              config.readonly = true;
+
+
 	
   }
 
+  private disableRate(){
+    if (this.isLoggedIn) {
+      
+      this.config.readonly = false;
+    } else {
+
+       this.config.readonly = true;
+
+    }
+  }
+  private onClick(value: Number) {
+     
+        console.log(this.etoile);
+
+   }
+
   ngOnInit() {
+
+    this.disableRate();
+
   }
 
 }
